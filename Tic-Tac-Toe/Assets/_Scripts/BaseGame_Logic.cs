305591f4434dd03d189b7d5dc2fa078622e1191b
RMalk <renaissancemalk@gmail.com>
2023-10-24 02:02:08 +0200

    BaseGame Update
    Strikeout lines
    Times

--- Tic-Tac-Toe/Assets/_Scripts/BaseGame_Logic.cs
+++ Tic-Tac-Toe/Assets/_Scripts/BaseGame_Logic.cs
@@ -6,87 +6,117 @@ using TMPro;
 
 public class BaseGame_Logic : MonoBehaviour
 {
-    public TMP_Text playerNumberText;
+    //TODO
+    int gridSize = 3;
 
-    public bool player = false;
+    public TMP_Text playerNumberText;
+    public TMP_Text timerText;
+
+    bool player = false;
 
     public bool[,] clicked = new bool[3,3];
     public bool[,] state = new bool[3,3];
 
-    int gridSize = 3;
+    //0 is vertical
+    //1 is horizontal
+    //2 is diagonal right
+    //3 is diagonal left
+    bool[] strikeout = new bool[4];
+    public Transform strikeoutLines;
 
-    public void Awake ()
+    float timer = 0;
+    bool gamePaused = false;
+
+    void Start()
     {
-
+        timer = 0;
     }
+
+    void Update()
+    {
+        if (!gamePaused)
+        {
+            timer += Time.deltaTime;
+            timerText.text = FactorTime();
+        }
+    }
+
+    string FactorTime()
+    {
+        int seconds = (int)(timer % 60);
+        int minutes = (int)Mathf.Floor(timer / 60);
+
+        return
+        (
+            (minutes < 10 ? "0" + minutes.ToString() : minutes.ToString())
+            + ":" +
+            (seconds < 10 ? "0" + seconds.ToString() : seconds.ToString())
+        );
+    }
+
 
     public void ButtonClick (BaseGame_TTTButton clickedButton)
     {
-        int x = clickedButton.x - 1;
-        int y = clickedButton.y - 1;
-
-
-        if (!clicked[x, y])
+        if (!gamePaused)
         {
-            int playerNumber = Convert.ToInt32(player);
+            int x = clickedButton.x - 1;
+            int y = clickedButton.y - 1;
 
-            clickedButton.transform.GetChild(playerNumber).gameObject.SetActive(true);
-
-            state[x, y] = player;
-
-            if (!CheckGameState(x, y))
+            if (!clicked[x, y])
             {
-                //Next player turn
-                player = !player;
-                //Change the text
-                playerNumber = Convert.ToInt32(player);
-                playerNumberText.text = (playerNumber + 1).ToString();
+                int playerNumber = Convert.ToInt32(player);
 
-                clicked[x, y] = true;
-            }
-            else
-            {
-                GameWin();
+                clickedButton.transform.GetChild(playerNumber).gameObject.SetActive(true);
+
+                state[x, y] = player;
+
+                if (!CheckGameState(x, y))
+                {
+                    //Next player turn switch
+                    player = !player;
+                    //Player text update
+                    playerNumber = Convert.ToInt32(player);
+                    playerNumberText.text = (playerNumber + 1).ToString();
+
+                    clicked[x, y] = true;
+                }
+                else
+                {
+                    GameWin(x, y);
+                }
             }
         }
     }
 
     bool CheckGameState(int x, int y)
     {
-        //0 is vertical, 1 is horizontal, 2 is right diagonal, 3 is left diagonal
-        bool[] gameWin = new bool[4];
-
+        //Check vertical
         int counter = 0;
 
-        //Check vertical
         for (int i = 0; i < gridSize; i++)
         {
             if (state[x, i] == player && clicked[x, i] == true)
                 counter++;
 
-            if (counter == gridSize-1)
-            {
-                gameWin[0] = true;
-            }
+            if (counter == gridSize -1 )
+                strikeout[0] = true;
         }
 
+        //Check horizontal
         counter = 0;
 
-        //Check horizontal
         for (int i = 0; i < gridSize; i++)
         {
             if (state[i, y] == player && clicked[i, y] == true)
                 counter++;
 
-            if (counter == gridSize-1)
-            {
-                gameWin[1] = true;
-            }
+            if (counter == gridSize - 1)
+                strikeout[1] = true;
         }
 
+        //Check diagonal right
         counter = 0;
 
-        //Check right diagonal
         if (x == y)
         {
             for (int i = 0; i < gridSize; i++)
@@ -95,15 +125,13 @@ public class BaseGame_Logic : MonoBehaviour
                     counter++;
 
                 if (counter == gridSize - 1)
-                {
-                    gameWin[2] = true;
-                }
+                    strikeout[2] = true;
             }
         }
 
+        //Check diagonal left
         counter = 0;
 
-        //Check left diagonal
         if ((x == 1 && y == 1) || Mathf.Abs(x - y) == 2 )
         {
             for (int i = 0; i < gridSize; i++)
@@ -112,31 +140,39 @@ public class BaseGame_Logic : MonoBehaviour
                     counter++;
 
                 if (counter == gridSize - 1)
-                {
-                    gameWin[3] = true;
-                }
+                    strikeout[3] = true;
             }
         }
 
         //Check Wins
         for (int i = 0; i < 4; i++)
-        {
-            if (gameWin[i])
+            if (strikeout[i])
                 return true;
-        }
 
         return false;
     }
 
-    void GameWin ()
+    void GameWin (int x, int y)
     {
-        Debug.Log("Player " + (Convert.ToInt32(player) + 1) + " Wins!");
+        gamePaused = true;
 
-        //ResetGame();
+        for (int i = 0; i < 4; i++)
+            strikeoutLines.GetChild(i).gameObject.SetActive(strikeout[i]);
+
+        if (strikeout[0])
+            strikeoutLines.GetChild(0).localPosition = new Vector3((x - 1) * 300, 0, 0);
+
+        if (strikeout[1])
+            strikeoutLines.GetChild(1).localPosition = new Vector3(0, (y - 1) * -300, 0);
+
+        //Debug.Log("Player " + (Convert.ToInt32(player) + 1) + " Wins!");
+        StartCoroutine(ResetGame());
     }
 
-    void ResetGame ()
+    IEnumerator ResetGame ()
     {
+        yield return new WaitForSeconds(1);
+
         for (int x = 0; x < gridSize; x++)
         {
             for (int y = 0; y < gridSize; y++)
@@ -146,7 +182,18 @@ public class BaseGame_Logic : MonoBehaviour
                 transform.GetChild(0).GetChild(y).GetChild(x).GetChild(0).gameObject.SetActive(false);
                 transform.GetChild(0).GetChild(y).GetChild(x).GetChild(1).gameObject.SetActive(false);
             }
-
         }
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (strikeout[i])
+            {
+                strikeout[i] = false;
+                strikeoutLines.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+
+        timer = 0;
+        gamePaused = false;
     }
 }
