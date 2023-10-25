07df64cb0b2514331f44924929af99bfd38dbdcb
RMalk <renaissancemalk@gmail.com>
2023-10-25 19:53:09 +0200

    Shader update and a bit of cleanup

--- Tic-Tac-Toe/Assets/_Scripts/MainMenu/SettingsPopup.cs
+++ Tic-Tac-Toe/Assets/_Scripts/MainMenu/SettingsPopup.cs
@@ -1,26 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class SettingsPopup : MonoBehaviour
 {
-
     public AudioMixer mainAudioMixer;
+
+    public Slider masterSlider, soundSlider, musicSlider;
+    public Toggle soundToggle, musicToggle;
+
 
     public void ToggleMaster(bool toggle)
     {
+        masterSlider.enabled = toggle;
 
+        soundSlider.enabled = toggle;
+        soundToggle.enabled = toggle;
+
+        musicSlider.enabled = toggle;
+        musicToggle.enabled = toggle;
+
+        if (toggle)
+        {
+            SetMasterVolume(masterSlider.value);
+        }
+        else
+        {
+            SetMasterVolume(masterSlider.minValue);
+        }
     }
 
     public void ToggleSound (bool toggle)
     {
-
+        soundSlider.enabled = toggle;
+        if (toggle)
+        {
+            SetSoundVolume(soundSlider.value);
+        }
+        else
+        {
+            SetSoundVolume(soundSlider.minValue);
+        }
     }
 
     public void ToggleMusic(bool toggle)
     {
-
+        musicSlider.enabled = toggle;
+        if (toggle)
+        {
+            SetMusicVolume(musicSlider.value);
+        }
+        else
+        {
+            SetMusicVolume(musicSlider.minValue);
+        }
     }
 
     public void SetMasterVolume(float volume)
@@ -28,12 +63,12 @@ public class SettingsPopup : MonoBehaviour
         mainAudioMixer.SetFloat("masterVolume", volume);
     }
 
-    public void SetMusicVolume(float volume)
+    public void SetSoundVolume(float volume)
     {
         mainAudioMixer.SetFloat("soundVolume", volume);
     }
 
-    public void SetSoundVolume(float volume)
+    public void SetMusicVolume(float volume)
     {
         mainAudioMixer.SetFloat("soundVolume", volume);
     }
